c36effa383ba5237238dfc996afae8ae19814b3b
Sumit Chouhan <sumitbhai847.com>
2026-09-01 13:52:00 +0530

    implement API and CRUD operations

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const app = require("./app");
+const app = require("./src/app");
 
 const PORT = 3000;
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,47 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
+const notes = [];
+//crud operation
+// api - post/notes
+app.post('/notes',(req,res)=>{
+    notes.push(req.body);
 
+    res.status(201).json({
+        message:"note created successfully"
+    })
+    
+})
+
+// api ->get/notes
+app.get('/notes',(req,res)=>{
+    res.status(200).json({
+        message:"notes fatched successfully",
+        notes:notes
+    })
+})
+
+// api ->delete /notes/:1
+app.delete('/notes/:index',(req,res)=>{
+    const index = req.params.index
+
+    delete notes[index]
+
+    res.status(200).json({
+        message:"note deleted successfully",
+    })
+})
+// api ->update /notes/:1
+app.patch('/notes/:index',(req,res)=>{
+    const index = req.params.index
+
+    const discription = req.body.discription
+    notes[index].discription = discription
+
+    res.status(200).json({
+        message:"note updated successfully",
+    })
+})
 module.exports = app;
