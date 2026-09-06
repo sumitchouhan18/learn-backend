26525a0fecbfd9346bbdb90a0728e5e97a0f511e
Sumit Chouhan <sumitbhai847.com>
2026-09-06 20:34:46 +0530

    feat: implement CRUD operations with database

--- 4-crudWithDB/src/app.js
+++ 4-crudWithDB/src/app.js
@@ -4,6 +4,53 @@ const noteModel = require('./models/note.model')
 const app = express();
 app.use(express.json());
 
+app.post('/notes',async(req,res)=>{
+    const data = req.body;
 
+    await noteModel.create({
+    title:data.title,
+    discription:data.discription,
+    })
+    res.status(201).json({
+        message:"note created successfully"
+    })
 
+})
+
+app.get('/notes',async(req,res)=>{
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        message:"note fetched successfully",
+        notes:notes
+    })
+})
+app.get('/notes',async(req,res)=>{
+    const notes = await noteModel.findOne({
+        title:"title-1"
+    })
+    res.status(200).json({
+        message:"note fetched successfully",
+        notes:notes
+    })
+})
+app.delete('/notes/:id',async(req,res)=>{
+    const id = req.params.id
+    
+    await noteModel.findOneAndDelete({
+        _id:id
+    })
+    res.status(200).json({
+        message:"note deleted successfully",
+    
+    })
+})
+app.patch('/notes/:id',async(req,res)=>{
+    const id = req.params.id
+    const discription = req.body.discription
+    await noteModel.findOneAndUpdate({_id:id},{discription:discription})
+    res.status(200).json({
+        message:"note updated successfully",
+    })
+})
 module.exports= app;
--- 4-crudWithDB/src/models/note.model.js
+++ 4-crudWithDB/src/models/note.model.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
-    title:String,
-    discription:string,
-})
+
+    title: {
+        type: String,
+    },
+
+    discription: {
+        type: String,
+    },
+
+});
 
 const noteModel = mongoose.model("note",noteSchema)
 module.exports = noteModel;
